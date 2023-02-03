469664f89fc4f66f9ea4e396a6c3c4f9328b867e
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2023-02-03 22:10:52 +0100

    black fix

--- backend/crud/user_crud.py
+++ backend/crud/user_crud.py
@@ -18,47 +18,41 @@ async def get_users(skip: int = 0, limit: int = 100) -> list[dict]:
 async def get_user(user_id: str) -> dict:
     user = await User.find_one({"_id": ObjectId(user_id)})
     if not user:
-        raise HTTPException(
-            status_code=404, detail=f"User with id: {user_id} not found.")
+        raise HTTPException(status_code=404, detail=f"User with id: {user_id} not found.")
     user["_id"] = str(user["_id"])
     return user
 
 
 async def update_user(user_id: str, user: UserUpdate) -> dict:
-    user_id = ObjectId(user_id)
-    if len(user) >= 1:
-        update_result = await User.update_one({"_id": user_id}, {"$set": user})
-        if update_result.modified_count == 1:
-            user = await User.find_one({"_id": user_id})
-            if (updated_student := await User.find_one({"_id": user_id})) is not None:
-                updated_student["_id"] = str(updated_student["_id"])
-                return updated_student
-    if (existing_student := await User.find_one({"_id": user_id})) is not None:
+    update_result = await User.update_one({"_id": ObjectId(user_id)}, {"$set": user})
+    if update_result.modified_count == 1:
+        user = await User.find_one({"_id": ObjectId(user_id)})
+        if (updated_student := await User.find_one({"_id": ObjectId(user_id)})) is not None:
+            updated_student["_id"] = str(updated_student["_id"])
+            return updated_student
+    if (existing_student := await User.find_one({"_id": ObjectId(user_id)})) is not None:
         existing_student["_id"] = str(existing_student["_id"])
         return existing_student
-    raise HTTPException(
-        status_code=404, detail=f"User with id: {user_id} not found.")
+    raise HTTPException(status_code=404, detail=f"User with id: {user_id} not found.")
 
 
 async def create_user(user: UserCreate) -> dict:
     try:
-        new_user = await User.insert_one(
-            user
-        )  # returns pymongo.results.InsertOneResult (contain only _id)
+        # returns pymongo.results.InsertOneResult (contain only _id)
+        new_user = await User.insert_one(user)
     except pymongo.errors.DuplicateKeyError:
         raise HTTPException(
             status_code=409,
             detail=f"User with such email: {user['email']} already exists.",
         )
-    user = await User.find_one({"_id": new_user.inserted_id})
+    created_user = await User.find_one({"_id": new_user.inserted_id})
 
-    return user
+    return created_user
 
 
 async def delete_user(user_id: str) -> dict:
     user = await User.find_one_and_delete({"_id": ObjectId(user_id)})
     if not user:
-        raise HTTPException(
-            status_code=404, detail=f"User with id: {user_id} not found.")
+        raise HTTPException(status_code=404, detail=f"User with id: {user_id} not found.")
     user["_id"] = str(user["_id"])
     return user
