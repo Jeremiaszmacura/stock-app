95cc9a311bd9014a9fd0e67c4cf63c29c7e5e87d
Jeremiasz Macura <jeremiasz.macura@globallogic.com>
2024-02-04 13:43:44 +0100

    move fronted to another repo

--- frontend/src/UserContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-
-export const TokenContext = createContext(null);
-export const UserContext = createContext(null);
-export const AdminContext = createContext(null);
